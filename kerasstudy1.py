5c11607e79c36ee743e90966a5712371258eacbf
qiangge73 <4722868@qq.com>
2019-05-09 16:56:51 +0800

    Add files via upload

---
+++ kerasstudy1.py
@@ -0,0 +1,21 @@
+#encoding = 'UTF-8'
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
+
+fashion_mnist = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+
+
+
+
+
+
